7b4ef7c8afdecb5ca1d12acb0569a83a98c8281d
Arne Brasseur <arne@arnebrasseur.net>
2013-07-19 17:21:38 +0200

    Quick tryout of the Rails Girls Berlin tutorial with Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = 'afca4fb6dd2118d2868e841513c5e3ee2961fb6dae0ee07a1825e83bb28798b496abd2ff274f6f37d66a2c6a2d71910c16c5c08aae52967820e719e91c97b1ba'
